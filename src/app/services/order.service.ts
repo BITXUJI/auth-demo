55a3307f0823bb8d60012e79c88eb6a916c3a0c4
bitxuji <bitxuji@gmail.com>
2023-11-08 21:40:03 +0900

    add Authorization field to the httpheader

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,14 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 
 @Injectable()
 export class OrderService {
+  private token: string = '';
 
   constructor(private http: HttpClient) {
+    this.token = localStorage.getItem('token') as string;
   }
 
   getOrders() {
-    return this.http.get('/api/orders');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+    return this.http.get('/api/orders', { headers });
   }
 }
